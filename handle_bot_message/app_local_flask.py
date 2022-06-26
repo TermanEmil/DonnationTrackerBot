feabbd0b6a5bc1af8e6b432b967755859a6e5de7
TermanEmil <terman.emil@gmail.com>
2022-06-26 22:29:41 +0200

    Initial setup

---
+++ handle_bot_message/app_local_flask.py
@@ -0,0 +1,23 @@
+import flask
+import telegram
+from flask import request
+
+from handle_message import handle_message
+from telegram_bot import get_bot
+
+
+app = flask.Flask(__name__)
+
+
+@app.post("/handle_bot_message")
+def handle_bot_message():
+    if flask.request.headers.get('content-type') == 'application/json':
+        update = telegram.Update.de_json(request.get_json(force=True), get_bot())
+        handle_message(update)
+
+    return '', 204
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+    print("test")
